45bc26cabd9d3ecab89c91c4deafd609796f200b
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-05 14:33:56 +0200

    Configured ESLint, Prettier, and TailwindCSS. Added `publie/assets` folder. Updated `favicon.ico`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,69 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from 'tailwindcss';
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1440px',
+      },
+    },
     extend: {
+      colors: {
+        'primary-500': '#877EFF',
+        'primary-600': '#5D5FEF',
+        'secondary-500': '#FFB620',
+        'tertiary-500': '#0095F6',
+        'off-white': '#D0DFFF',
+        red: '#FF5A5A',
+        'dark-1': '#000000',
+        'dark-2': '#09090A',
+        'dark-3': '#101012',
+        'dark-4': '#1F1F22',
+        'light-1': '#FFFFFF',
+        'light-2': '#EFEFEF',
+        'light-3': '#7878A3',
+        'light-4': '#5C5C7B',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      screens: {
+        xs: '480px',
+      },
+      width: {
+        '420': '420px',
+        '465': '465px',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      keyframes: {
+        'accordion-down': {
+          // @ts-expect-error
+          from: {height: 0},
+          to: {height: 'var(--radix-accordion-content-height)'},
+        },
+        'accordion-up': {
+          from: {height: 'var(--radix-accordion-content-height)'},
+          // @ts-expect-error
+          to: {height: 0},
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('tailwindcss-animate')],
+};
+export default config;
