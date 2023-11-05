4c032e176a22d005abf4401623926899e587eddc
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-05 15:31:58 +0200

    Implemented `createUserAccount`, `saveUserToDB`, and `signInAccount` appwrite actions

---
+++ appwrite/actions/user.action.ts
@@ -0,0 +1,62 @@
+import {INewUser} from '@/types';
+import appwriteConfig from '../conf';
+import {account, avatars, database, ID} from '../client';
+
+export async function createUserAccount(user: INewUser) {
+  try {
+    const newAccount = await account.create(ID.unique(), user.email, user.password, user.name);
+
+    if (!newAccount) {
+      throw new Error('User not created');
+    }
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      email: newAccount.email,
+      name: newAccount.name,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+
+    return newUser;
+  } catch (error: any) {
+    console.error(error);
+  }
+}
+
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await database.createDocument(
+      appwriteConfig.databaseId as string,
+      appwriteConfig.userCollectionId as string,
+      ID.unique(),
+      user
+    );
+
+    if (!newUser) {
+      throw new Error('User not saved to DB');
+    }
+
+    return newUser;
+  } catch (error: any) {
+    console.error(error);
+  }
+}
+
+export async function signInAccount(user: {email: string; password: string}) {
+  try {
+    const session = await account.createEmailSession(user.email, user.password);
+
+    return session;
+  } catch (error: any) {
+    console.error(error);
+  }
+}
