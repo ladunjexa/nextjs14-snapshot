bb44a45da3b7fc467c29de81cea730ebc7951197
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-05 20:37:54 +0200

    Refactored header text to dynmically display search results when a query is present

--- app/(root)/explore/page.tsx
+++ app/(root)/explore/page.tsx
@@ -4,7 +4,15 @@ import Image from 'next/image';
 import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
 import LocalResult from '@/components/shared/search/LocalResult';
 
-export default function Page() {
+type Props = {
+  searchParams: {[key: string]: string | undefined};
+};
+
+export default function Page({searchParams}: Props) {
+  const query = searchParams.q;
+
+  console.log(query);
+
   return (
     <div className="explore-container">
       <div className="explore-inner_container">
@@ -14,7 +22,9 @@ export default function Page() {
       </div>
 
       <div className="flex-between mb-7 mt-16 w-full max-w-5xl">
-        <h3 className="body-bold md:h3-bold">Popular Today</h3>
+        <h3 className="body-bold md:h3-bold">
+          {!query ? 'Popular Today' : `Search results for "${query}"`}
+        </h3>
 
         <div className="flex-center cursor-pointer gap-3 rounded-xl bg-dark-3 px-4 py-2">
           <p className="small-medium md:base-medium text-light-2">All</p>
