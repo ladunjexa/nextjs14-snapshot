d04c5689c79dbe774f7ac26e7965e9776ec71974
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-05 19:42:10 +0200

    Implemented dynamic route for edit profile page

---
+++ app/(root)/edit-profile/[id]/page.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+import Profile from '@/components/forms/Profile';
+
+type Props = {
+  params: {id: string};
+};
+
+export default function Page({params}: Props) {
+  return (
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="flex-start w-full max-w-5xl justify-start gap-3">
+          <Image src="/assets/icons/add-post.svg" width={36} height={36} alt="add" />
+          <h2 className="h3-bold md:h2-bold w-full text-left">Edit Profile</h2>
+        </div>
+        <Profile userId={params.id} />
+      </div>
+    </div>
+  );
+}
