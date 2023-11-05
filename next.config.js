4d6630cdc051574da99278ad4054d15cfb8342ab
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-05 16:15:41 +0200

    Introduced image configuration for @appwrite 's remote patterns in `next.config.js`

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+      },
+      {
+        protocol: 'http',
+        hostname: 'cloud.appwrite.io',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
