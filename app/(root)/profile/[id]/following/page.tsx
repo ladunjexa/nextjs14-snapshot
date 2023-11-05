9d2898f41c19a4f66b518c34ed11c7d193b10be0
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-05 21:53:20 +0200

    Implemented following and followers list pages

---
+++ app/(root)/profile/[id]/following/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Follows from '@/components/scenes/Follows';
+
+type Props = {
+  params: {id: string};
+};
+
+export default function Page({params}: Props) {
+  return <Follows viewedId={params.id} type="Following" />;
+}
