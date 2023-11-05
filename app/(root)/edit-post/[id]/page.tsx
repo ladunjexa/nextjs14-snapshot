4b01d3a337f8349065719384fc939d39be6e49a2
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-05 17:05:21 +0200

    Implemented dynamic route for editing posts

---
+++ app/(root)/edit-post/[id]/page.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+import Post from '@/components/forms/Post';
+
+type Props = {
+  params: {id: string};
+};
+
+export default function Page({params}: Props) {
+  return (
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="flex-start w-full max-w-5xl justify-start gap-3">
+          <Image src="/assets/icons/add-post.svg" width={36} height={36} alt="add" />
+          <h2 className="h3-bold md:h2-bold w-full text-left">Edit Post</h2>
+        </div>
+
+        <Post action="Update" postId={params.id} />
+      </div>
+    </div>
+  );
+}
