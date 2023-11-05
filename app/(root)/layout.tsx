3214cac4eea636b3c938fd8033abccb0c0acdc6a
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-05 16:21:01 +0200

    Implemented `Bottombar` layout-component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Bottombar from '@/components/shared/layout/Bottombar';
 import LeftSidebar from '@/components/shared/layout/LeftSidebar';
 import Topbar from '@/components/shared/layout/Topbar';
 import React from 'react';
@@ -11,7 +12,7 @@ export default function Layout({children}: {children: React.ReactNode}) {
       <section className="flex h-full flex-1">{children}</section>
 
       {/* <RightSidebar /> */}
-      {/* <Bottombar /> */}
+      <Bottombar />
     </main>
   );
 }
---
+++ components/shared/layout/Bottombar.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import {usePathname} from 'next/navigation';
+
+import {bottombarLinks} from '@/constants';
+
+import type {INavLink} from '@/types';
+
+const Bottombar = () => {
+  const pathname = usePathname();
+
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link: INavLink) => {
+        const isActive =
+          (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+
+        return (
+          <Link
+            href={link.route}
+            key={link.label}
+            className={`flex-center flex-col gap-1 p-2 transition ${
+              isActive && 'rounded-[10px] bg-primary-500'
+            }`}
+          >
+            <Image
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={`${isActive && 'invert-white'}`}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Bottombar;
