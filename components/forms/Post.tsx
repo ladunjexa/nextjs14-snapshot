f42f9a6e64486e7e96c0a5099bdeeee5031d24b8
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-05 16:28:35 +0200

    Implemented initial version of `Post` form-component

---
+++ components/forms/Post.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import React from 'react';
+
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {z} from 'zod';
+
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from '@/components/ui/form';
+import {Input} from '@/components/ui/input';
+import {Button} from '@/components/ui/button';
+import {Textarea} from '@/components/ui/textarea';
+
+import {PostValidation} from '@/lib/validations';
+
+const Post = () => {
+  const form = useForm<z.infer<typeof PostValidation>>({
+    resolver: zodResolver(PostValidation),
+    defaultValues: {
+      caption: '',
+      file: [],
+      location: '',
+      tags: '',
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof PostValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full max-w-5xl flex-col gap-9">
+        <FormField
+          control={form.control}
+          name="caption"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Caption</FormLabel>
+              <FormControl>
+                <Textarea className="shad-textarea custom-scrollbar" {...field} />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="file"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Add Photos</FormLabel>
+              <FormControl>{/* <FileUploader /> */}</FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="location"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Add Location</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">
+                Add Tags (separated by comma &quot; , &quot;)
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  className="shad-input"
+                  placeholder="Art, Expression, Learn"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+        <div className="flex items-center justify-end gap-4">
+          <Button type="button" className="shad-button_dark_4">
+            Cancel
+          </Button>
+          <Button type="submit" className="shad-button_primary whitespace-nowrap">
+            Create Post
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default Post;
