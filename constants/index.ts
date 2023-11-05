134fe820a0068eda744b59af59623074fccbf98e
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-05 16:18:28 +0200

    Introduced ts module `constants/index.ts` for centralize definitions of navigation links

---
+++ constants/index.ts
@@ -0,0 +1,52 @@
+import type {INavLink} from '@/types';
+
+export const sidebarLinks: INavLink[] = [
+  {
+    imgURL: '/assets/icons/home.svg',
+    route: '/',
+    label: 'Home',
+  },
+  {
+    imgURL: '/assets/icons/wallpaper.svg',
+    route: '/explore',
+    label: 'Explore',
+  },
+  {
+    imgURL: '/assets/icons/people.svg',
+    route: '/all-users',
+    label: 'People',
+  },
+  {
+    imgURL: '/assets/icons/bookmark.svg',
+    route: '/saved',
+    label: 'Saved',
+  },
+  {
+    imgURL: '/assets/icons/gallery-add.svg',
+    route: '/create-post',
+    label: 'Create Post',
+  },
+];
+
+export const bottombarLinks: INavLink[] = [
+  {
+    imgURL: '/assets/icons/home.svg',
+    route: '/',
+    label: 'Home',
+  },
+  {
+    imgURL: '/assets/icons/wallpaper.svg',
+    route: '/explore',
+    label: 'Explore',
+  },
+  {
+    imgURL: '/assets/icons/bookmark.svg',
+    route: '/saved',
+    label: 'Saved',
+  },
+  {
+    imgURL: '/assets/icons/gallery-add.svg',
+    route: '/create-post',
+    label: 'Create',
+  },
+];
